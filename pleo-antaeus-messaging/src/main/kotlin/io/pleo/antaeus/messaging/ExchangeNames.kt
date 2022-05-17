f6a3e9b89c62173eca5e03dbab5b08c8d0763d18
sergio <moyano.serrano@gmail.com>
2022-05-17 17:22:55 +0200

    Deleted unnecessary classes

--- pleo-antaeus-messaging/src/main/kotlin/io/pleo/antaeus/messaging/ExchangeNames.kt
+++
@@ -1,7 +0,0 @@
-package io.pleo.antaeus.messaging
-
-class ExchangeNames {
-    companion object {
-        val customersExchange : String = "customersExchange"
-    }
-}
--- pleo-antaeus-messaging/src/main/kotlin/io/pleo/antaeus/messaging/QueueNames.kt
+++
@@ -1,8 +0,0 @@
-package io.pleo.antaeus.messaging
-
-class QueueNames {
-    companion object {
-        val pendingInvoiceQueue: String = "PendingInvoiceQueue"
-        val customersToInvoiceQueue : String = "CustomersToInvoiceQueue"
-    }
-}
